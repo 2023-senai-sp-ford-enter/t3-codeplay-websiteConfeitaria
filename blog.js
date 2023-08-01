672c16febb364d078e2e767130eba567fb3a841e
SarahCarvalho-S <sarah.carvalho027@gmail.com>
2023-07-31 21:50:31 -0300

    atualização 31.07

--- blog.js
+++ blog.js
@@ -1,4 +1,4 @@
-function inserir() {
+/* function inserir() {
     
     let itemUsuario = document.getElementById('entradaUsuario').value;
     let textNode = document.createTextNode(itemUsuario);
@@ -10,6 +10,24 @@ function inserir() {
 
     elementoPaiUl.appendChild(elementoFilhoLi);
 
-}
+}*/
 
 
+function comentar() {
+    const inputName = document.getElementById('inputName');
+    const textComment = document.getElementById('textComment');
+    const form = document.getElementById('formulario');
+    const commentPost = document.getElementById('comentario');
+
+    // form.addEventListener('click', (event) => {
+        event.preventDefault(); //**para impedir que a página recarregue
+
+        let p = document.createElement('p');
+        p.classList = 'd-flex text-wrap flex-wrap';
+        p.innerHTML = `<strong>${inputName.value}: &nbsp </strong> ${textComment.value}`; 
+        commentPost.appendChild(p);
+        inputName.value = '';
+        textComment.value = '';
+        inputName.focus();
+
+    };
